a8d2a228f115dbc3b48806af9b57aa5bc2808d64
fyui001 <yun.cod@gmail.com>
2020-09-19 17:05:10 +0900

    テーブル追加

--- docker/db/create_table.sql
+++ docker/db/create_table.sql
@@ -8,3 +8,14 @@ CREATE TABLE `masturbation_log` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=225 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `ghosts` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `ghost_id` int(10) unsigned NOT NULL COMMENT '幽霊屋敷のユーザーID',
+  `gold` bigint(20) unsigned NOT NULL DEFAULT '9000' COMMENT 'サーバー内通貨(みつは)',
+  `del_flg` tinyint(1) NOT NULL DEFAULT '0' COMMENT '削除フラグ',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNQ_GHOST_ID` (`ghost_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
