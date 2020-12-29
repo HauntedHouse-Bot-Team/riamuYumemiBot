08ce51068d362160319528786583d7a0a45588c7
fyui001 <yun.cod@gmail.com>
2020-12-29 21:10:36 +0900

    削除機能を搭載

--- src/UserModules.py
+++ src/UserModules.py
@@ -11,8 +11,25 @@ class UserModules:
     def all_member_register(self, members: list):
 
         columns = [
+            'name',
             'ghost_id',
             'gold',
         ]
 
         self.db.multiple_insert(self.table, columns, members)
+
+    def member_delete(self, member_id: int):
+        columns = [
+            'del_flg',
+        ]
+        values = [
+            1,
+        ]
+        narrow_down_column = [
+            'ghost_id',
+        ]
+        narrow_down_values = [
+            member_id,
+        ]
+
+        self.db.update(self.table, columns, values, narrow_down_column, narrow_down_values)
